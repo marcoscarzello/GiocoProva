ca34e23b7354d7d1ac6eefc33a623345fcc2ba72
DESKTOP-7HGJO8N\Scarzello <s286366@studenti.polito.it>
2021-05-10 23:44:01 +0200

    importato ParrelSinc

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+
+    //prova variabile sincronizzata
+    [SyncVar]
+    int asd = 0;
+
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
+
+            transform.position = transform.position + movement;
+
+        }
+    }
+
+    void Update()
+    {
+        HandleMovement();
+
+        if (isLocalPlayer && Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Salutando il server");
+            Hola();
+        }
+
+    }
+    [Command]
+    void Hola()
+    {
+        Debug.Log("ciao dal client");
+    }
+
+    void OnContatoreChange(int old, int nuovo) { }
+}
