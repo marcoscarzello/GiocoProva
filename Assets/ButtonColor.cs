4fb30b76225e6502bc1ef2e175ae08527eef8c3c
Lara Fantone <lara.fantone@studenti.polito.it>
2021-05-26 12:33:59 +0200

    scenaLara

---
+++ Assets/ButtonColor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonColor : MonoBehaviour
+{
+    public GameObject panel;
+   
+
+    public void ChangeButtonColor()
+    {
+        if (panel.activeSelf)
+        {
+           
+        }
+    }
+
+}
