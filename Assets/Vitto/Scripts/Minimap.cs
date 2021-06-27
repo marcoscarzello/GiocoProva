a171397aea810421f89d16fd6a82da283b865832
vitto <s287686@studenti.polito.it>
2021-06-27 16:48:43 +0200

    cosemappe

--- Assets/Vitto/Scripts/Minimap.cs
+++ Assets/Vitto/Scripts/Minimap.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Minimap : MonoBehaviour
 {
-    private int state=0; /*1- db     2- mappa armi e nemico lv1      3- lv2      4- lv3     5- timer torna all'ascensore */
+    private int state=0; /*1- db     2- mappa armi e nemico lv1      3- lv2 , lv3      4- lv3     5- timer torna all'ascensore */
     private int power = 0; //stati powerup
 
     private Animator animator = null;
@@ -50,14 +50,18 @@ public class Minimap : MonoBehaviour
         {
             case 1:
                 db.SetActive(true);
+                gun.SetActive(true);
+                //gun2.SetActive(true);
+                gunsIcons(-1);
                 break;
             case 2:
-                gun.SetActive(true);
+                db.SetActive(false);
                 lv1.SetActive(true);
                 break;
             case 3:
                 lv1.SetActive(false);
                 lv2.SetActive(true);
+                lv3.SetActive(true);
                 break;
             case 4:
                 lv2.SetActive(false);
@@ -70,7 +74,35 @@ public class Minimap : MonoBehaviour
         }
     }
 
-    void setTime(int time)
+    private void gunsIcons(int i)
+    {
+        //Transform t = getGunsTransform();
+        if(i>-1)
+            switch (i)
+            {
+                case 1:
+                    gun.SetActive(false);
+                    break;
+                case 2:
+                    //gun2.SetActive(false);
+                    break;
+                case 3:
+                    //gun3.SetActive(false);
+                    break;
+                case 4:
+                    //gun4.SetActive(false);
+                    break;
+            }
+        else
+        {
+            //gun1.transform.position = t.position;
+            //gun2.transform.position = t.position;
+            //gun3.transform.position = t.position;
+            //gun4.transform.position = t.position;
+        }
+    }
+
+    private void setTime(int time)
     {
         Time.timeScale = time;
         /*if (time == 0)
